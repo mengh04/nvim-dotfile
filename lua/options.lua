e758ebc66c60f6d04e8ecd1324c6d9c37270a880
mengh04 <mengh0412@hotmail.com>
2025-01-21 07:14:20 +0800

    添加了markdown插件

--- lua/options.lua
+++ lua/options.lua
@@ -33,10 +33,11 @@ vim.opt.wrap = false
 
 if vim.g.neovide then
     vim.g.neovide_hide_mouse_when_typing = true
-    vim.g.neovide_confirm_quit = true
+    vim.g.neovide_confirm_quit = false
     --vim.g.neovide_cursor_smooth_blink = true;
     vim.g.neovide_padding_top = 0
     vim.g.neovide_padding_bottom = 0
     vim.g.neovide_padding_right = 0
     vim.g.neovide_padding_left = 0
+    vim.o.guifont = "Hack Nerd Font:h14" -- text below applies for VimScript
 end
