00fdf3ce117656bc4b2b176d7fb44c95e3f21f41
dreamhaze04 <mengh0412@hotmail.com>
2025-01-23 16:47:33 +0800

    linux

--- lua/options.lua
+++ lua/options.lua
@@ -38,5 +38,9 @@ if vim.g.neovide then
     vim.g.neovide_padding_bottom = 0
     vim.g.neovide_padding_right = 0
     vim.g.neovide_padding_left = 0
+<<<<<<< HEAD
     vim.o.guifont = "FiraCode Nerd Font:h14" -- text below applies for VimScript
+=======
+    -- vim.o.guifont = "Hack Nerd Font:h14" -- text below applies for VimScript
+>>>>>>> cfdc7d0 (linux)
 end
