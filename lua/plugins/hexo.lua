533b3e4e861d7e9fa11e83b2df989527383e8e0f
dreamhaze04 <mengh0412@hotmail.com>
2025-01-25 17:40:16 +0800

    linux

--- lua/plugins/hexo.lua
+++ lua/plugins/hexo.lua
@@ -2,7 +2,7 @@ return {
   -- 加载本地插件
   {
     "hexo", -- 插件名
-    dir = "/home/mengh04/.config/nvim/lua/my_plugins", -- 本地路径，指向包含插件的目录
+    dir = vim.fn.stdpath("config") .. "/lua/my_plugins", -- 使用 Neovim 配置路径
     config = function()
       require("my_plugins.hexo").setup() -- 加载并初始化插件
     end,
