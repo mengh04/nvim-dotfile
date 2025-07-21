4ef60cab86eeee7c8bae07667444426aefa414a7
mengh04 <mengh0412@hotmail.com>
2025-07-21 13:37:37 +0800

    update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,12 +3,13 @@ return {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' },
+        event = 'VimEnter',
         -- 使用 keys 实现懒加载和清晰的配置
         keys = {
-            { '<leader>ff', function() require('telescope.builtin').find_files() end,  desc = 'Find Files' },
-            { '<leader>fg', function() require('telescope.builtin').live_grep() end,   desc = 'Live Grep' },
-            { '<leader>fb', function() require('telescope.builtin').buffers() end,     desc = 'Find Buffers' },
-            { '<leader>fh', function() require('telescope.builtin').help_tags() end,   desc = 'Help Tags' },
+            { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Find Files' },
+            { '<leader>fg', function() require('telescope.builtin').live_grep() end,  desc = 'Live Grep' },
+            { '<leader>fb', function() require('telescope.builtin').buffers() end,    desc = 'Find Buffers' },
+            { '<leader>fh', function() require('telescope.builtin').help_tags() end,  desc = 'Help Tags' },
         },
         -- config 函数只用来做 setup，保持纯净
         config = function()
